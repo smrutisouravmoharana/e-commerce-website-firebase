fc93893c2b0c8bf45a8eb705b0c2eda6fb14bf2e
smrutisouravmoharana <ssmoharana2023@gmail.com>
2024-07-01 20:33:32 +0530

    sourav chnages

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import Home from "./pages/home/Home";
-import Order from "./pages/Order/Order";
+import Order from './pages/order/Order';
 import Cart from "./pages/cart/Cart";
 import Dashboard from "./pages/admin/dashboard/Dashboard";
 import NoPage from "./pages/nopage/NoPage";
